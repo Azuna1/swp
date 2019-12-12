337b53ab264f48afb8e1663609b9725f694b7385
JD <refstylelt@googlemail.com>
2019-12-12 14:48:12 +0100

    working SMTP sendMail()

--- myportal/src/main/java/swp/portal/MainView.java
+++ myportal/src/main/java/swp/portal/MainView.java
@@ -1,6 +1,9 @@
 package swp.portal;
 
+import java.io.UnsupportedEncodingException;
+
 import javax.inject.Inject;
+import javax.mail.MessagingException;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -11,6 +14,7 @@ import com.vaadin.flow.server.PWA;
 import antlr.debug.Event;
 import swp.dao.UserDAO;
 import swp.usecase.impl.GeraeteManager;
+import swp.usecase.impl.MailManager;
 
 /**
  * The main view contains a button and a click listener.
@@ -23,42 +27,61 @@ public class MainView extends VerticalLayout {
     private MessageBean messageBean;
 	
     public MainView() {
-        Button button = new Button("Click me",
-                event -> UserDAO.doit());
-        add(button);
+//        Button button = new Button("Click me",
+//                event -> UserDAO.doit());
+//        add(button);
         
-        //zum Testen Buttons anlegen
+        //TesField für Create Geraete
         
-        //@Inject
-    	GeraeteManager geraeteManager = new GeraeteManager();
-    	
-        TextField tf1 = new TextField ("Beschreibung");
-        TextField tf2 = new TextField("Kategorie");
-        TextField tf3 = new TextField("Preis");
-        TextField tf4 = new TextField("Geraetename");
-        add(tf1, tf2, tf3, tf4);
-        /*
-        String var1 = tf1.getValue();
-        String var2 = tf2.getValue();
-        double var3 = Double.valueOf(tf3.getValue());
-        String var4 = tf4.getValue();
-        */
+//        //@Inject
+//    	GeraeteManager geraeteManager = new GeraeteManager();
+//    	
+//        TextField tf1 = new TextField ("Beschreibung");
+//        TextField tf2 = new TextField("Kategorie");
+//        TextField tf3 = new TextField("Preis");
+//        TextField tf4 = new TextField("Geraetename");
+//        add(tf1, tf2, tf3, tf4);
+//        
+//        String var1 = tf1.getValue();
+//        String var2 = tf2.getValue();
+//        double var3 = Double.valueOf(tf3.getValue());
+//        String var4 = tf4.getValue();
+//        
+//        Button buttonTest = new Button("testy Send",
+//                event -> geraeteManager.createGeraet(var1,var2,var3,var4));
+//        add(buttonTest);
+//     
         
-        String var1="0",var2="0",var4="0";
-        double var3=0;
+     // Mail Testing Zone ^-^
+        TextField tf1 = new TextField ("Message");
+        add(tf1);
+        MailManager mailManager = new MailManager();
+        String message;
+        message=tf1.getValue();
         
-        Button buttonTest = new Button("testy Send",
-                event -> geraeteManager.createGeraet(var1,var2,var3,var4));
-        add(buttonTest);
+      //Login aufm SMTP von Google
         
+        
+        Button b1 = new Button("Login on SMTP",
+        	Event -> {	
+        		mailManager.loginToMail();
+        		
+        	});
+        
+      Button b2 = new Button("versende mich",
+      event -> {
+		try {
+			//Send der Email
+			mailManager.sendMail(message);
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	});
+      add(b1,b2);
+        	}
     }
-    
- 
-    
-    
-        
-    
-    
-    
-    
-}
+
--- myportal/src/main/java/swp/usecase/impl/MailManager.java
+++ myportal/src/main/java/swp/usecase/impl/MailManager.java
@@ -1,40 +1,81 @@
 package swp.usecase.impl;
-
-import java.net.PasswordAuthentication;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.Properties;
-import java.util.*;
-
-import javax.jms.Session;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 //@Stateless
 public class MailManager {
 
-	String toFibu ="refstylelt@googlemail.com";
-	String toStudentString ="refstylelt@googlemail.com";
-	String toAdminString ="refstylelt@googlemail.com";
+	protected Session mailSession;	
 	
-	
-	final String userName ="sendmeamailswp2019";
+	public void loginToMail() {
+		
+	//login Daten sollten irgendwo hin outgesourced werden!!
+	final String userName ="sendmeamailswp2019@gmail.com";
 	final String password ="Softwareprojekt2019!";
 	
 	String host ="smtp.gmail.com";
-	String port="587";
+	String port="465"; //SSL Port
+	//String port="587"; //TLS Port
 	
 	Properties props = new Properties();
 	props.put("mail.smtp.auth","true");
-	props.put("mail.smtp.starttls.enable","true");
+	//props.put("mail.smtp.starttls.enable","true");
+	props.put("mail.smtp.socketFactory.port",port);
+	props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
 	props.put("mail.smtp.host",host);
 	props.put("mail.smtp.port",port);
 	
-	//Session erstellen
-	
-	Session session = Session.getInstance(props,new javax.mail.Authenticator() {
+	Authenticator auth = new Authenticator() {
+		@Override
 		protected PasswordAuthentication getPasswordAuthentication() {
 			return new PasswordAuthentication(userName, password);
 		}
-	});
-	//Message an sich -> auch in Strings packen -> 3verschiedene je nach Usecase
+	};
 	
+	this.mailSession = Session.getDefaultInstance(props, auth);
+	System.out.println("Login erfolgreich!!!");
 	
+	}
+	
+	public void sendMail(String message) throws MessagingException, UnsupportedEncodingException {
+		
+		String toFibu ="refstylelt@googlemail.com";
+		String toStudentString ="refstylelt@googlemail.com"; //Email muss aus dem LDAP kommen...
+		String toAdminString ="refstylelt@googlemail.com";
+		String absenderEmail ="sendmeamailswp2019@gmail.com";
+		String absenderName ="Mail Man";
+		String subject = "TestMail";
+		//String message = "";
+		
+		if(mailSession == null) {
+			loginToMail();
+		}
+		MimeMessage msg = new MimeMessage(mailSession);
+		msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+		msg.addHeader("Content-Transfer-Encoding", "8bit");
+		
+		msg.setFrom(new InternetAddress(absenderEmail, absenderName));
+		msg.setReplyTo(InternetAddress.parse(absenderEmail, false));
+		msg.setSubject(subject,"UTF-8");
+		msg.setText(message,"UTF-8");
+		msg.setSentDate(new Date());
+		
+		//Angabe,wohin die Mail(s) hingesendet werden sollen -> auch mehrere Nennungen möglich
+		msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toFibu, false));
+	
+		System.out.println("Versende EMail an: "+toFibu);
+		
+		Transport.send(msg);
+		System.out.println("Email wurde versandt! ");
+	}
 	
 }
